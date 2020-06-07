4d208cfa40408bbd65f6995d8f4213a595dd6887
vagrant <matchball.shine@gmail.com>
2020-06-07 08:01:30 +0000

    [finish]chapter6

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,24 +5,47 @@ namespace App\Http\Controllers;
 use App\Folder;
 use App\Task;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
-  public function index(int $id)
-{
-    // すべてのフォルダを取得する
-    $folders = Folder::all();
+    public function index(int $id)
+  {
+      // すべてのフォルダを取得する
+      $folders = Folder::all();
 
-    // 選ばれたフォルダを取得する
-    $current_folder = Folder::find($id);
+      // 選ばれたフォルダを取得する
+      $current_folder = Folder::find($id);
 
-    // 選ばれたフォルダに紐づくタスクを取得する
-    $tasks = $current_folder->tasks()->get(); // ★
+      // 選ばれたフォルダに紐づくタスクを取得する
+      $tasks = $current_folder->tasks()->get(); // ★
 
-    return view('tasks/index', [
-        'folders' => $folders,
-        'current_folder_id' => $current_folder->id,
-        'tasks' => $tasks,
-    ]);
-}
+      return view('tasks/index', [
+          'folders' => $folders,
+          'current_folder_id' => $current_folder->id,
+          'tasks' => $tasks,
+      ]);
+  }
+
+  public function showCreateForm(int $id)
+  {
+      return view('tasks/create', [
+          'folder_id' => $id
+      ]);
+  }
+
+  public function create(int $id, CreateTask $request)
+  {
+      $current_folder = Folder::find($id);
+
+      $task = new Task();
+      $task->title = $request->title;
+      $task->due_date = $request->due_date;
+
+      $current_folder->tasks()->save($task);
+
+      return redirect()->route('tasks.index', [
+          'id' => $current_folder->id,
+      ]);
+  }
 }
